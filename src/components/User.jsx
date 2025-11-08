52c830130a062cf0a01edd1fbd67f7294cc4a01c
Mahadi9990 <mdshafayat9999@gmail.com>
2025-11-08 22:38:26 +0600

    base done

---
+++ src/components/User.jsx
@@ -0,0 +1,39 @@
+import React, { use, useState } from "react";
+
+export default function User({ userInfo }) {
+  const initialUser = use(userInfo);
+  const [user, setuser] = useState(initialUser);
+  console.log(user)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const newUser = { name, email };
+    fetch("http://localhost:3000/usersPost", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then(res => res.json())
+      .then(data => {
+        const newUser = [...user,data]
+        setuser(newUser)
+        e.target.reset()
+      });
+  };
+  return (
+    <>
+      <div className="">
+        <h3>User form</h3>
+        <form onSubmit={handleSubmit}>
+          <input name="name" type="text" placeholder="user name " />
+          <input name="email" type="email" placeholder="email" />
+          <button>Submit</button>
+        </form>
+      </div>
+      <div>{user.map((item) => item.name)}</div>
+    </>
+  );
+}
